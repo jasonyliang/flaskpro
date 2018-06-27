dbe745c065ec23d9289940db7e05e631540039b9
Jason Liang <jjjasonliang@gmail.com>
2018-06-27 15:40:29 +0900

    standard page

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
 from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return "<h1>Hello! World.</h1>"
+
+
+@app.route('/homepage')
+def homepage():
+    return "This is a homepage xDD"
+
+
+
+if __name__ == "__main__":
+    app.run()
