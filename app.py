b4c509c2c253bc0e2e759ba1c7a7492932171fac
Jason Liang <jjjasonliang@gmail.com>
2018-06-28 11:52:17 +0900

    database link

--- app.py
+++ app.py
@@ -1,8 +1,24 @@
 from flask import Flask, render_template
-
-
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:roottoor@localhost/sample'
+
+db = SQLAlchemy(app)
+
+class Usser(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(80))
+    email = db.Column(db.String(100), unique = True)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+
 
 
 @app.route('/')
