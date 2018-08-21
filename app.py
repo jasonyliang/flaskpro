8b08961be6de236b850975eb47b82ec6b16d6223
Jason Liang <jjjasonliang@gmail.com>
2018-08-21 15:13:07 +0800

    form

--- app.py
+++ app.py
@@ -6,10 +6,11 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:roottoor@localhos
 
 db = SQLAlchemy(app)
 
+
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
+    id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80))
-    email = db.Column(db.String(100), unique = True)
+    email = db.Column(db.String(100), unique=True)
 
     def __init__(self, username, email):
         self.username = username
@@ -19,21 +20,20 @@ class User(db.Model):
         return '<User %r>' % self.username
 
 
-
-
 @app.route('/')
 def index():
-    users = [
-                {
-                    'name': "jhajhajhajha", 
-                    "username": "jhajhajha11"
-                },
-                {
-                    'name': "John",
-                    'username': "Johnny"
-                },
-            ]
-    return render_template('index.html', users = users)
+    # users = [
+    #             {
+    #                 'name': "jhajhajhajha",
+    #                 "username": "jhajhajha11"
+    #             },
+    #             {
+    #                 'name': "John",
+    #                 'username': "Johnny"
+    #             },
+    #         ]
+    # return render_template('index.html', users = users)
+    return render_template('add_user.html')
 
 
 if __name__ == "__main__":
